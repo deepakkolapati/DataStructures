a957559e9d94e015d0759adbcbdf860007cd85b7
deepakkolapati <deepakchandu9@gmail.com>
2025-09-02 19:39:12 +0530

    created classes for Tree Node and LinkedList Nodes

--- src/main/java/com/deepak/binaryTree/TreeNode.java
+++ src/main/java/com/deepak/binaryTree/TreeNode.java
@@ -1,5 +1,18 @@
 package com.deepak.binaryTree;
 
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
 }
---
+++ src/main/java/com/deepak/linkedList/Node.java
@@ -0,0 +1,10 @@
+package com.deepak.linkedList;
+
+public class Node {
+    int val;
+    Node next;
+
+    Node(int val){
+        this.val = val;
+    }
+}
